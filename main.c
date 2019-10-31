13b608d9ae75787b1634d0306f63249c6907f4ae
Woojung618 <eyes_cat@naver.com>
2019-10-31 15:36:07 +0900

    3

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int i; int grade[SIZE]; int score[SIZE];
+	int i; /*int grade[SIZE]; int score[SIZE];
 	for (i=0;i<5;i++){
 		grade[i]=rand()%100;
 		score[i]=grade[i];
@@ -12,7 +12,14 @@ int main(int argc, char *argv[]) {
 	for (i=0;i<5;i++){
 		printf("score[%d]=%d\n",i,score[i]);
 	}
-	
+	*/
+	int a[5]={1,2,3,4,5};
+	int b[5]={1,2,3,5,5};
+	for (i=0;i<5;i++){
+		if (a[i]!=b[i]){
+			printf("%i is different \n",i);
+		}
+	}
 	
 
 	
